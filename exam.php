0362c642fc9c4d4ef9652009eddfe70e59960d01
oluwaseunisrael <111712379+oluwaseunisrael@users.noreply.github.com>
2024-09-23 04:10:11 +0100

    Add files via upload

---
+++ exam.php
@@ -0,0 +1,8 @@
+<?php
+$God ="All glory to you, Jesus of Nazareth";
+$Jesus = "Thank you for the incredible success" ;
+$Holyspirit = "over these past four years.";
+$output="You are the reason it happened";
+echo $God . " " . $Jesus ." ". $Holyspirit ." ". $output;
+
+?>
